c89b37925c529bcdce92f6ddda7a6f51af04602c
Haddad Sarah <haddads686@gmail.com>
2022-04-24 01:33:10 +0100

    the web site is almost done

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ const navbar = {
   button: "",
   nav: document.querySelector(".main-nav"),
 };
+const schedule = {
+  buttons: document.querySelectorAll(".schedule-nav a"),
+  days: document.querySelectorAll(".schedule-day"),
+};
 
 function scrollHandler(e) {
   if (window.pageYOffset > 300) navbar.nav.classList.add("fixed");
@@ -12,3 +16,20 @@ function scrollHandler(e) {
 }
 document.onscroll = scrollHandler;
 scrollHandler();
+
+function hideDays() {
+  schedule.days.forEach((elem) => {
+    elem.classList.remove("active");
+  });
+  schedule.buttons.forEach((elem) => {
+    elem.classList.remove("active");
+  });
+}
+
+schedule.buttons.forEach(function (elem, i) {
+  elem.onclick = function () {
+    hideDays();
+    elem.classList.add("active");
+    schedule.days[i].classList.add("active");
+  };
+});
